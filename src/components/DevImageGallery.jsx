61df8d724a66f62f53b76351b06baf2c39e137d0
Alex Abainza <abainza.alexandra@gmail.com>
2024-06-27 17:19:57 +0800

    added new project

--- src/components/DevImageGallery.jsx
+++ src/components/DevImageGallery.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import apptive from "../assets/ApptiveMain.png";
 import fitness from "../assets/fitnessdemo.jpg";
+import budgify from "../assets/portfolio/budgify.jpg";
 
 import { SocialIcon } from "react-social-icons";
 import GalleryImage from "./GalleryImage";
@@ -25,8 +26,7 @@ const DevImageGallery = () => {
         description:
           "The Fitness Tracking App's major purpose is to help users take charge of their health and fitness. The software encourages people to create and achieve fitness goals by providing an easy-to-use interface and a robust set of functions. From tracking workout progress to monitoring changes in body measures, this application is an efficient tool for anyone looking to improve their entire well-being.",
         github_link: "https://github.com/alexabainza/fitness-tracking-app",
-        figma_link:
-          "https://www.figma.com/file/s5YiRjVQxjsN9jP3yKfsXc/UI%2FUX-APPTIVE?type=design&node-id=347%3A27&mode=design&t=1iEzZSuR6AlImYBD-1",
+        figma_link: "/",
         image: fitness,
         demo_link:
           "https://drive.google.com/file/d/1cKTCLJWJD9gCQcqomkmzm43cca0Rybp0/view?usp=sharing",
@@ -38,6 +38,18 @@ const DevImageGallery = () => {
           "ExpressJS",
           "MySQL",
         ],
+      },
+      {
+        title: "Finances Tracking App",
+        description:
+          "A mobile development course project that allows users to manage their budget by allowing CRUD operations and features.",
+        github_link: "https://github.com/alexabainza/expo-budgify",
+        figma_link:
+          "https://www.figma.com/design/C4eQhsUx9BHIc83yrU5SU6/MobDev-final-project?node-id=0-1&t=NiUwwB7ewWE5GBvF-1",
+        image: budgify,
+        demo_link:
+          "https://drive.google.com/file/d/1gS4rSV523IV_OaczqIYna2H5hDRmXbXx/view?usp=sharing",
+        category: ["React Native", "Firebase"],
       },
     ],
   };
